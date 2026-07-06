c5b6ebd3ce234195e5a217bbd6025a8150224086
soyLibraco <soylibraco@gmail.com>
2026-07-06 20:22:46 -0300

    Crea carpeta modulos para aprender a usar import

---
+++ modulos/import_3.py
@@ -0,0 +1,4 @@
+import operaciones as op
+
+op.sumar(5, 4)
+op.restar(10, 3)
---
+++ modulos/operaciones.py
@@ -0,0 +1,14 @@
+def sumar(a, b):
+    print(a + b)
+
+def restar(a, b):
+    print(a - b)
+
+def multiplicar(a, b):
+    print(a * b)
+
+def dividir(a, b):
+    if b != 0:
+        print(a / b)
+    else:
+        print("Error: División por cero")
