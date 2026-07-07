c3d4212443d250433cb6fae73313f86ec0c6195c
soyLibraco <soylibraco@gmail.com>
2026-07-06 21:45:11 -0300

    Realiza packages_ejercicio

---
+++ packages_ejercicio/cliente/datos.py
@@ -0,0 +1,29 @@
+base_datos = {
+    "clientes": [
+        {
+            "Nombre": "Hector",
+            "Apellido": "Costa",
+            "Direccion": "Calle Las Flores 123",
+            "Telefono": "15554234",
+            "Email": "hector@gmail.com",
+            "Preferido": "True",
+            "Balance": "100",
+        },
+        {
+            "Nombre": "Juan",
+            "Apellido": "Perez",
+            "Direccion": "Calle Las Rosas 456",
+            "Telefono": "15554235",
+            "Email": "juan@gmail.com",
+            "Preferido": "False",
+            "Balance": "200",
+        },
+    ],
+    "empleados": [
+        {
+            "Nombre": "Ana",
+            "Apellido": "Gomez",
+        },
+        {"Nombre": "Maria", "Apellido": "Pérez"},
+    ],
+}
